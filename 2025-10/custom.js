c0a95363706f1fc281010a5c18de1caa0eec5af8
Raman Sharma <82493061+ramansharma694@users.noreply.github.com>
2025-10-26 19:27:59 -0700

    Update custom.js

--- 2025-10/custom.js
+++ 2025-10/custom.js
@@ -22,7 +22,7 @@ jQuery(document).ready(function() {
             const headlines = [
                 {
                     type: "full",
-                    text: "Nisġa'a is rooted deeply in the land and sea"
+                    text: "Nisg̱a’a is rooted deeply in the land and sea"
                 },
                 {
                     type: "full",
@@ -30,7 +30,7 @@ jQuery(document).ready(function() {
                 },
                 {
                     type: "partial",
-                    staticText: "Nisġa'a is our ",
+                    staticText: "Nisg̱a’a is our ",
                     words: ["community", "history", "heart"]
                 }
             ];
